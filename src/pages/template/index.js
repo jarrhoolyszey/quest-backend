0f4bacc74c397e7e7c8eebc06c83d70f912bc77c
jarrhoolyszey <jarrhoolyszey@gmail.com>
2021-09-25 18:02:03 -0300

    pages/template adicionado

---
+++ src/pages/template/index.js
@@ -0,0 +1,35 @@
+const { response } = require("express");
+
+(function(w, d, qns){
+  w.addEventListener('load', () => {
+    
+    /* Código JS aqui ... */
+
+    // Modelo de requisição: 
+    // try {
+      // Modelo de requisição do tipo GET
+      // const response = await qns.api.get('url');
+
+      // Modelo de requisição do tipo POST
+      // const response = await qns.api.post('url', {
+         // json
+      //});
+      
+      // const data = response.data;
+
+      // tratamento da resposta
+
+    // } catch (error) {
+      // err.response.data to get server message
+      // console.log(error);
+    // }
+
+    // Criação de elementos dinamicos JS
+    // Tratar array de objetos
+    
+    //let item = d.createElement('li')
+    //item.setAttribute("data-id", lista[n]._id)
+    //d.appendChild('ul')
+
+  })
+})(window, document, window.qns = window.qns || {});
